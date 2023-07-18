4e04db61e6b8cee3010ea82f1ddcae799c874d7c
Kilsik <ilsiwow@gmail.com>
2023-07-18 15:04:20 +0300

    Initial commit

---
+++ foodcartapp/models.py
@@ -0,0 +1,181 @@
+from django.db import models
+from django.core.validators import MinValueValidator
+
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Restaurant(models.Model):
+    name = models.CharField(
+        'название',
+        max_length=50
+    )
+    address = models.CharField(
+        'адрес',
+        max_length=100,
+        blank=True,
+    )
+    contact_phone = models.CharField(
+        'контактный телефон',
+        max_length=50,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'ресторан'
+        verbose_name_plural = 'рестораны'
+
+    def __str__(self):
+        return self.name
+
+
+class ProductQuerySet(models.QuerySet):
+    def available(self):
+        products = (
+            RestaurantMenuItem.objects
+            .filter(availability=True)
+            .values_list('product')
+        )
+        return self.filter(pk__in=products)
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(
+        'название',
+        max_length=50
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(
+        'название',
+        max_length=50,
+        db_index=True,
+    )
+    category = models.ForeignKey(
+        ProductCategory,
+        verbose_name='категория',
+        related_name='products',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        db_index=True,
+    )
+    price = models.DecimalField(
+        'цена',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    image = models.ImageField(
+        'картинка'
+    )
+    special_status = models.BooleanField(
+        'спец.предложение',
+        default=False,
+        db_index=True,
+    )
+    description = models.TextField(
+        'описание',
+        max_length=200,
+        blank=True,
+    )
+
+    objects = ProductQuerySet.as_manager()
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+
+    def __str__(self):
+        return self.name
+
+
+class RestaurantMenuItem(models.Model):
+    restaurant = models.ForeignKey(
+        Restaurant,
+        related_name='menu_items',
+        verbose_name="ресторан",
+        on_delete=models.CASCADE,
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='menu_items',
+        verbose_name='продукт',
+    )
+    availability = models.BooleanField(
+        'в продаже',
+        default=True,
+        db_index=True
+    )
+
+    class Meta:
+        verbose_name = 'пункт меню ресторана'
+        verbose_name_plural = 'пункты меню ресторана'
+        unique_together = [
+            ['restaurant', 'product']
+        ]
+
+    def __str__(self):
+        return f"{self.restaurant.name} - {self.product.name}"
+
+
+class Order(models.Model):
+    order_datetime = models.DateTimeField(
+        'дата и время поступления заказа',
+        auto_now=True,
+    )
+    address = models.CharField(
+        'адрес',
+        max_length=300,
+        db_index=True,
+    )
+    name = models.CharField(
+        'имя',
+        max_length=100,
+    )
+    surname = models.CharField(
+        'фамилия',
+        max_length=200,
+        blank=True,
+    )
+    phone = PhoneNumberField(
+        'телефон',
+    )
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return f'{self.name} {self.surname} - {self.address}'
+
+
+class OrderProducts(models.Model):
+    order = models.ForeignKey(
+        Order,
+        verbose_name='заказ',
+        on_delete=models.CASCADE,
+        related_name='products',
+    )
+    product = models.ForeignKey(
+        Product,
+        verbose_name='пункт заказа',
+        related_name='orders',
+        on_delete=models.DO_NOTHING,
+        null=True,
+    )
+    quantity = models.PositiveIntegerField(
+        'количество',
+    )
+
+    class Meta:
+        verbose_name = 'пункт заказа'
+        verbose_name_plural = 'пункты заказа'
---
+++ foodcartapp/views.py
@@ -0,0 +1,93 @@
+import json
+
+from django.http import JsonResponse
+from django.templatetags.static import static
+from phonenumber_field.phonenumber import PhoneNumber
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Product, Order, OrderProducts
+
+
+def banners_list_api(request):
+    # FIXME move data to db?
+    return JsonResponse([
+        {
+            'title': 'Burger',
+            'src': static('burger.jpg'),
+            'text': 'Tasty Burger at your door step',
+        },
+        {
+            'title': 'Spices',
+            'src': static('food.jpg'),
+            'text': 'All Cuisines',
+        },
+        {
+            'title': 'New York',
+            'src': static('tasty.jpg'),
+            'text': 'Food is incomplete without a tasty dessert',
+        }
+    ], safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
+
+
+def product_list_api(request):
+    products = Product.objects.select_related('category').available()
+
+    dumped_products = []
+    for product in products:
+        dumped_product = {
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'special_status': product.special_status,
+            'description': product.description,
+            'category': {
+                'id': product.category.id,
+                'name': product.category.name,
+            } if product.category else None,
+            'image': product.image.url,
+            'restaurant': {
+                'id': product.id,
+                'name': product.name,
+            }
+        }
+        dumped_products.append(dumped_product)
+    return JsonResponse(dumped_products, safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
+
+
+@api_view(['POST'])
+def register_order(request):
+    try:
+        data = request.data
+    except ValueError as err:
+        print(f'Произошла ошибка {err}')
+    if not data['products']:
+        print('Невозможно сделать пустой заказ')
+        return Response({})
+    address = data['address']
+    first_name = data['firstname']
+    last_name = data['lastname']
+    phone = PhoneNumber.from_string(data['phonenumber'])
+    order = Order.objects.create(
+        address=address,
+        name=first_name,
+        surname=last_name,
+        phone=phone
+    )
+    for product in data['products']:
+        product_id = product['product']
+        quantity = product['quantity']
+        order_product = Product.objects.get(pk=product_id)
+        possition = OrderProducts.objects.get_or_create(
+            order=order,
+            product=order_product,
+            quantity=quantity,
+        )
+    print(data)
+    return Response({})
